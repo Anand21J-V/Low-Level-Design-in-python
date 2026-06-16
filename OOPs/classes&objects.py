1090d55b46b474849d9e0c56867970e80289d623
Anand21J-V <anandvishwakarma21j.com>
2026-06-16 12:22:10 +0530

    Basic Class and objects Creation

---
+++ OOPs/classes&objects.py
@@ -0,0 +1,17 @@
+# Revision of classes and objects and OOPs concepts
+
+# Class Creation
+class Animal:
+    def __init__(self, name: str, age: int) -> None:  # Initializer method (Will get called when object is created)
+        self.name = name
+        self.age = age
+
+    # Method for displaying the attributes of the object
+    def display(self) -> None:
+        print(f"Name of the Animal is {self.name} and the age is {self.age}")    
+
+# Object Creation
+dog = Animal("Maxxy", 5)
+print(dog)
+dog.display()
+
